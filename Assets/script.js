842af2e71bb5524e1fd19b63add6093f80ede892
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-03-10 10:00:18 -0500

    Updated readme, tried to set up local storage in js

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,33 @@
-//set up local storage
+//Set up save to local storage.
+/*$(document).ready(function)()  {
+console.log(localStorage.getItem('text'))
+$('#saveBtn').on('click', function(e) {
+console.log($(document.getElementById('text'))
+})
+})
 
-//var calEntry =$("#textarea")
-//saveBtn.addEventListener('click'),function(event)
-//event.click.saveBtn;
+$(function () {
+    $('text').click(function () {
+        var name = $('text').val();
+        alert(text);
+        localStorage.setItem('text',text);
+    });
+});
+
+// get the text
+var text = $('#test').text();
+// set the item in localStorage
+localStorage.setItem('test', text);
+// bind text to 'click' event for div
+$('#test').on('click', function() {
+    // check the new text
+    var newText = $(this).text();
+    // overwrite the old text
+    localStorage.setItem('test', newText);
+    // test if it works
+    alert(localStorage.getItem('test'));
+});*/
+
+
+
 
